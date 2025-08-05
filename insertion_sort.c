dd13464a3e6eba0f4ec790d2789986224bdf1f67
Purva Patel <purvapatel9855@gmail.com>
2025-08-05 15:26:46 +0530

    committed

---
+++ insertion_sort.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int Insertion_sort(int arr[], int n) {
+    int i, j, temp;
+    for (i = 1; i < n; i++) {
+        temp=arr[i];
+        j=i-1;
+        while(j>=0 && arr[j]>temp){
+            arr[j+1]=arr[j];
+            j--;
+        }
+        arr[j+1]=temp;
+    }
+}
+
+int main() {
+    int n, i;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    
+    int arr[n];
+    printf("Enter the elements:\n");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    Insertion_sort(arr, n);
+
+    printf("Array in ascending order:\n");
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
---
+++ selection_sort.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+void Selection_sort(int arr[], int n) {
+    int i, j, min, temp;
+    for (i = 0; i < n - 1; i++) {
+        min = i;
+        for (j = i + 1; j < n; j++) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+        }
+        if (min != i) {
+            temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
+    }
+}
+
+int main() {
+    int n, i;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    
+    int arr[n];
+    printf("Enter the elements:\n");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    Selection_sort(arr, n);
+
+    printf("Array in ascending order:\n");
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }    
+    return 0;
+}
