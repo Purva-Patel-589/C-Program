2361e8824e69ac2cc6ed550312fd2b9b4a47ce5c
Purva Patel <purvapatel9855@gmail.com>
2025-08-28 17:16:17 +0530

    committed

--- oops/exno2c.cpp
+++ oops/exno2c.cpp
@@ -1,4 +1,5 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
 using namespace std;
 
 class CustomerAccount {
@@ -7,50 +8,38 @@ private:
     string firstName;
     string lastName;
     string panNumber;
-    string accountNumber;
-    double balance;   
+    long long accountNumber;
+    const double balance;
 
 public:
-    CustomerAccount() {
-        bankName = "State Bank of India"; 
-        firstName = ""; 
-        lastName = ""; 
-        panNumber = ""; 
-        accountNumber = ""; 
-        balance = 0.0;
+    CustomerAccount() : balance(0.0) {
+        bankName = "State Bank of India";  
+        firstName = "";
+        lastName = "";
+        panNumber = "";
+        accountNumber = 0;
     }
 
-    CustomerAccount(string bName, string fName, string lName,string pan, string accNo, double bal){
-        bankName = bName;
-        firstName = fName;
-        lastName = lName; 
-        panNumber = pan;
-        accountNumber = accNo;
-        balance = bal;
-    }
+    CustomerAccount(string bName, string fName, string lName, string pan, long long accNo, double bal)
+        : bankName(bName), firstName(fName), lastName(lName), panNumber(pan), accountNumber(accNo), balance(bal) {}
 
-    void displayDetails() {
-        cout << "---------------------------------\n";
+    void displayDetails() const {
+        cout << "----- Customer Account Details -----\n";
         cout << "Bank Name      : " << bankName << endl;
-        cout << "Account Holder : " << firstName << " " << lastName << endl;
+        cout << "First Name     : " << firstName << endl;
+        cout << "Last Name      : " << lastName << endl;
         cout << "PAN Number     : " << panNumber << endl;
         cout << "Account Number : " << accountNumber << endl;
-        cout << "Balance        : $" << balance << endl;
-        cout << "---------------------------------\n";
+        cout << "Balance        : " << balance << endl;
+        cout << "-----------------------------------\n";
     }
 };
 
 int main() {
-    CustomerAccount myAccount("HDFC Bank", "Purva", "Patel", "ABCDE1234F", "1234567890", 50000.75); 
-                              
+    CustomerAccount myAccount("HDFC Bank", "Purva", "Patel", "ABCDE1234F", 9876543210, 50000.75);
+    CustomerAccount familyAccount("ICICI Bank", "Ramesh", "Patel", "XYZAB6789K", 1234567890, 75000.50);
 
-    CustomerAccount familyAccount("ICICI Bank", "Rahul", "Patel", "XYZAB6789K", "9876543210", 75000.50);
-                                  
-
-    cout << "My Account Details:\n";
     myAccount.displayDetails();
-
-    cout << "\nFamily Member Account Details:\n";
     familyAccount.displayDetails();
 
     return 0;
